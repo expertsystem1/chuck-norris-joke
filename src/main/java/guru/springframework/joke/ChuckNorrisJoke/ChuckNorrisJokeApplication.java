aca2629056d7f80d5d2d1ea0841ec416321e6235
Vincenzo <vincenzolucente@gmail.com>
2020-03-24 09:52:12 +0100

    service created

--- src/main/java/guru/springframework/joke/ChuckNorrisJoke/ChuckNorrisJokeApplication.java
+++ src/main/java/guru/springframework/joke/ChuckNorrisJoke/ChuckNorrisJokeApplication.java
@@ -3,11 +3,14 @@ package guru.springframework.joke.ChuckNorrisJoke;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+
 @SpringBootApplication
 public class ChuckNorrisJokeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChuckNorrisJokeApplication.class, args);
+		
 	}
 
 }
---
+++ src/main/java/guru/springframework/joke/ChuckNorrisJoke/services/impl/JokeServiceImpl.java
@@ -0,0 +1,22 @@
+package guru.springframework.joke.ChuckNorrisJoke.services.impl;
+
+import org.springframework.stereotype.Service;
+
+import guru.springframework.joke.ChuckNorrisJoke.services.JokeService;
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+
+@Service
+public class JokeServiceImpl implements JokeService{
+	
+	private final ChuckNorrisQuotes chuckNorrisQuotes;
+
+	public JokeServiceImpl() {
+		this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+	}
+
+	@Override
+	public String getJoke() {
+		return chuckNorrisQuotes.getRandomQuote();
+	}
+
+}
