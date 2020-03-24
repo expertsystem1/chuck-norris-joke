452e1497bf647e133dcae503c2fed2894fd84be2
Vincenzo <vincenzolucente@gmail.com>
2020-03-24 09:40:49 +0100

    project created with dependecies

---
+++ src/test/java/guru/springframework/joke/ChuckNorrisJoke/ChuckNorrisJokeApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.joke.ChuckNorrisJoke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChuckNorrisJokeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
