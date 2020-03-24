dd9ba261251a9cefe7344e6a1f901fe55e68173d
Vincenzo <vincenzolucente@gmail.com>
2020-03-24 17:40:17 +0100

    configuration by XML

--- src/main/java/guru/springframework/joke/ChuckNorrisJoke/ChuckNorrisJokeApplication.java
+++ src/main/java/guru/springframework/joke/ChuckNorrisJoke/ChuckNorrisJokeApplication.java
@@ -2,10 +2,12 @@ package guru.springframework.joke.ChuckNorrisJoke;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
 
 @SpringBootApplication
+@ImportResource("classpath:chuck-config.xml")
 public class ChuckNorrisJokeApplication {
 
 	public static void main(String[] args) {
--- src/main/java/guru/springframework/joke/ChuckNorrisJoke/config/ChuckJokeConfig.java
+++ src/main/java/guru/springframework/joke/ChuckNorrisJoke/config/ChuckJokeConfig.java
@@ -1,14 +1,11 @@
 package guru.springframework.joke.ChuckNorrisJoke.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
 
-@Configuration
+
 public class ChuckJokeConfig {
    
-   @Bean
+  
    public ChuckNorrisQuotes chuckNorrisQuotes() {
 	   return new ChuckNorrisQuotes();
    }
